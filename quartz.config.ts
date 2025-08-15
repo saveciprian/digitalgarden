e78ba43c4edbbcc9b8e12f3743ef46bd44dc9f08
Tudor Savescu <60888561+saveciprian@users.noreply.github.com>
2025-08-15 14:47:30 +0200

    Quartz sync: Aug 15, 2025, 2:47 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Tudor's Hyperlink Extravaganza",
+    pageTitle: "Hyperlink Extravaganza",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
